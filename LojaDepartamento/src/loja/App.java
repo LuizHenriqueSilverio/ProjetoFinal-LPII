f6fcc8cdf3420f733c5f203d0c8015292291f349
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-03-15 07:35:03 -0300

    Primeiro commit.

---
+++ LojaDepartamento/src/loja/App.java
@@ -0,0 +1,22 @@
+package loja;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 02239263610
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
