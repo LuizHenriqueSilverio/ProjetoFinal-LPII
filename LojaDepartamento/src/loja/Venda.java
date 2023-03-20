c2d2e871c2b9da9842b75aca871c1ea792b45208
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-03-20 08:05:30 -0300

    Classe Venda implementada.

---
+++ LojaDepartamento/src/loja/Venda.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loja;
+
+/**
+ *
+ * @author luizh
+ */
+public class Venda {
+    private Cliente cliente;
+    private Produto produto;
+    private double qtd;
+    private Funcionario funcionario;
+    private double valorVenda;
+    private String dataVenda;
+
+    public Venda(Cliente cli, Produto prod, double qtd, Funcionario func, double valor, String data) {
+        this.cliente = cli;
+        this.produto = prod;
+        this.qtd = qtd;
+        this.funcionario = func;
+        this.valorVenda = valor;
+        this.dataVenda = data;
+    }
+    
+    /**
+     * @return the cliente
+     */
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    /**
+     * @param cliente the cliente to set
+     */
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    /**
+     * @return the produto
+     */
+    public Produto getProduto() {
+        return produto;
+    }
+
+    /**
+     * @param produto the produto to set
+     */
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    /**
+     * @return the qtd
+     */
+    public double getQtd() {
+        return qtd;
+    }
+
+    /**
+     * @param qtd the qtd to set
+     */
+    public void setQtd(double qtd) {
+        this.qtd = qtd;
+    }
+
+    /**
+     * @return the funcionario
+     */
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    /**
+     * @param funcionario the funcionario to set
+     */
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    /**
+     * @return the valorVenda
+     */
+    public double getValorVenda() {
+        return valorVenda;
+    }
+
+    /**
+     * @param valorVenda the valorVenda to set
+     */
+    public void setValorVenda(double valorVenda) {
+        this.valorVenda = valorVenda;
+    }
+
+    /**
+     * @return the dataVenda
+     */
+    public String getDataVenda() {
+        return dataVenda;
+    }
+
+    /**
+     * @param dataVenda the dataVenda to set
+     */
+    public void setDataVenda(String dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+    
+    
+}
